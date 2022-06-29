8c0c4cf4e23cdd4ff8670c5a280659c828a815b6
iamyanbo <86802756+iamyanbo@users.noreply.github.com>
2022-06-29 14:38:17 -0400

    Chatroom styling

--- client/spotchat/src/components/ChatRoom.tsx
+++ client/spotchat/src/components/ChatRoom.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
+import CardHeader from 'react-bootstrap/esm/CardHeader';
 import { io } from 'socket.io-client';
 import { NavbarComponent } from './Navbar';
 
@@ -9,17 +10,44 @@ class ChatRoom extends React.Component<{}, any> {
         super(props);
         this.state = {
             user: JSON.parse(localStorage.getItem('user')!),
+            userId: JSON.parse(localStorage.getItem('user')!).userId,
             text:"",
             chats: [],
             channelId: window.location.pathname.split('/')[2],
             socket: io('ws://localhost:5000', {
-                transports: ['websocket']})
+                transports: ['websocket']}),
+            styleSelf: {
+                backgroundColor: '#3a5ebf',
+                borderRadius: '5px',
+                color: 'white',
+                padding: '5px',
+                margin: '5px',
+                width: 'fit-content',
+                clear: 'both',
+                float: 'right',
+                marginRight: '20%',
+                maxWidth: '30%'
+            },
+            styleOther: {
+                backgroundColor: '#f5f5f5',
+                borderRadius: '5px',
+                color: 'black',
+                padding: '5px',
+                margin: '5px',
+                width: 'fit-content',
+                clear: 'both',
+                float: 'left',
+                marginLeft: '20%',
+                maxWidth: '30%'
+            }
         }
         this.state.socket.on(this.state.channelId, (data: any) => {
             this.setState({ chats: [...this.state.chats, data] });
         });
         this.handleTextChange = this.handleTextChange.bind(this);
         this.componentDidMount = this.componentDidMount.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.scroll = this.scroll.bind(this);
     }
 
     async componentDidMount() {
@@ -27,8 +55,16 @@ class ChatRoom extends React.Component<{}, any> {
         this.setState({chats: channel.data.messages});
     }
 
+    scroll = () => {
+        window.scroll(0, document.body.scrollHeight);
+    }
+
+    componentDidUpdate() {
+        this.scroll();
+    }
+
     handleTextChange = (event: any) => {
-        if (event.keyCode === 13) {
+        if (event.keyCode === 13 && this.state.text !== "") {
             const message = {
                 user: {
                     userId: this.state.user.userId,
@@ -48,26 +84,58 @@ class ChatRoom extends React.Component<{}, any> {
         }   
     }
 
+    handleSubmit = () => {
+        if (this.state.text !== "") {
+            const message = {
+                user: {
+                    userId: this.state.user.userId,
+                    name: this.state.user.aboutMe.display_name,
+                    avatar: this.state.user.profilePicture
+                },
+                message: this.state.text,
+                timestamp: new Date().getTime()
+            }
+            this.setState({text: ""});
+            axios.patch("http://localhost:8080/channels", {
+                message: message,
+                channelId: this.state.channelId
+            });
+        }
+    }
+
     render() {
         return (
             <div className="ChatRoom">
                 <NavbarComponent />
-                <Button href="/chatList" variant="primary">Back</Button>
-                <section>
+                <Button href="/chatList" variant="outline-secondary" style={{margin: "10px", position: "fixed"}}>Back to Chat List</Button>
+                <section style={{display: "inline-block", width: "100%", marginTop:"5%", marginBottom:"5%"}}>
                     {this.state.chats.map((chat: any, index: number) => (
                         <div className="chat-message" key={index}>
-                            <div className="chat-message-user">{chat.user.name}</div>
-                            <div className="chat-message-text">{chat.message}</div>
+                            <Card style={chat.user.userId === this.state.userId ? this.state.styleSelf : this.state.styleOther}>
+                                <Card.Header>
+                                    <img src={chat.user.avatar} alt="avatar" style={{width: "30px", height: "30px", borderRadius: "50%"}} />
+                                    <span style={{margin: "0px 10px"}}>{chat.user.name}</span>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        {chat.message}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                         </div>
                     ))}
-                    <input
-                type="text"
-                value={this.state.text}
-                placeholder="chat here..."
-                className="form-control"
-                onChange={this.handleTextChange}
-                onKeyDown={this.handleTextChange}
-            />
+                    <div style={{textAlign: "center", position: "fixed", bottom: "0px", width: "100%", backgroundColor: "#f5f5f5", display:"flex"}}>
+                        <input
+                            style={{margin: "10px", borderRadius: "5px", padding: "5px", width:"100%", marginLeft:"20%"}}
+                            type="text"
+                            value={this.state.text}
+                            placeholder="chat here..."
+                            className="form-control"
+                            onChange={this.handleTextChange}
+                            onKeyDown={this.handleTextChange}
+                        />
+                        <Button variant="outline-primary" style={{margin: "10px", borderRadius: "5px", padding: "5px", marginRight:"20%"}} onClick={this.handleSubmit}>Send</Button>
+                    </div>
             </section>
           </div>
         );
